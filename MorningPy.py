582e4068765a9cb07438a6201ba3f2ead41643c7
Johan Korsnes <johan.korsnes@gmail.com>
2016-09-26 20:30:41 +0200

    Stupid XML parsing

--- MorningPy.py
+++ MorningPy.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import requests # Brukes til å sende og hente.
 import bs4 # Håndtering av XML
-
+import xml.etree.ElementTree as ET # Alternativt bibliotek for XML-parsing
 
 SOAP_ACTION_GET_STOP_MONITORING = "GetStopMonitoring"
 NODE_MONITORED_VEHICLE_JOURNEY = "MonitoredVehicleJourney"
@@ -9,6 +11,31 @@ SIRI_SM_SERVICE = "http://st.atb.no/SMWS/SMService.svc"
 SIRI_NAMESPACE = "http://www.siri.org.uk/siri"
 
 STOP_ID = "16010011"
+
+def getArrivalTimes(tree, location):
+    """Input: XML-tree and name of bus stop. Has to be the name used in AtB's XML.
+    Output: A list of existing aimed arrival times."""
+    root = tree.getroot()
+    StopMonitoringDelivery = root[0][0][1][0]
+
+    # remove all non-MonitoredStopVisit-elements from tree:
+    MonitoredStopVisits = []
+    for deliveryElements in StopMonitoringDelivery:
+        if deliveryElements.tag.find("MonitoredStopVisit") != -1:
+            MonitoredStopVisits.append(deliveryElements)
+
+    # remove all MonitoredStopVisits with incorrect location:
+    ElementsOfInterest = []
+    for elem in MonitoredStopVisits:
+        if elem[2][6].text.find(location) != -1:
+            ElementsOfInterest.append(elem)
+
+    # create list of arrival times
+    TimeTable = []
+    for elem in ElementsOfInterest:
+        TimeTable.append(elem[2][13][6].text)
+
+    return TimeTable
 
 def getEnvelope(stopId, namespace):
     return ("<S:Envelope xmlns:S='http://schemas.xmlsoap.org/soap/envelope/' xmlns:s='http://www.siri.org.uk/siri' xmlns:b='" + namespace + "'>" +
@@ -50,8 +77,8 @@ def busStopForecast(stopId,namespace,url):
     f.close()
 
 def main():
-    busStopForecast(STOP_ID,SIRI_NAMESPACE,SIRI_SM_SERVICE)
+    tree = ET.parse('AtB.xml')
+    print(getArrivalTimes(tree, 'Pirbadet'))
 
 if __name__ == '__main__':
     main()
-
