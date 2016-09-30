965af4fe95e3646fd01223882972b7ec05843dd2
Johan Korsnes <johan.korsnes@gmail.com>
2016-09-30 21:31:23 +0200

    XML for Åsveien. Working on XML-parsing...

--- MorningPy.py
+++ MorningPy.py
@@ -3,6 +3,8 @@
 import requests # Brukes til å sende og hente.
 import bs4 # Håndtering av XML
 import xml.etree.ElementTree as ET # Alternativt bibliotek for XML-parsing
+from collections import namedtuple
+import time
 
 SOAP_ACTION_GET_STOP_MONITORING = "GetStopMonitoring"
 NODE_MONITORED_VEHICLE_JOURNEY = "MonitoredVehicleJourney"
@@ -10,7 +12,79 @@ NODE_MONITORED_VEHICLE_JOURNEY = "MonitoredVehicleJourney"
 SIRI_SM_SERVICE = "http://st.atb.no/SMWS/SMService.svc"
 SIRI_NAMESPACE = "http://www.siri.org.uk/siri"
 
-STOP_ID = "16010011"
+STOP_ID = "16010576"
+
+TimeStruct = namedtuple('TimeStruct', ['Orig', 'Real'])
+
+def getDepartureTimesAsveien(tree):
+    root = tree.getroot()
+
+    # Navigate the tree (consider collecting loops)
+    curChild = None
+    for child in root:
+        if child.tag[-4:] == "Body":
+            curChild = child
+
+    for child in curChild:
+        if child.tag == "GetStopMonitoringResponse":
+            curChild = child
+
+    for child in curChild:
+        if child.tag == "Answer":
+            curChild = child
+
+    for child in curChild:
+        if child.tag == "StopMonitoringDelivery":
+            curChild = child
+
+    # Extract the "buses"
+    buses = []
+    for child in curChild:
+        if child.tag == "MonitoredStopVisit":
+            for subChild in child:
+                if subChild.tag == "MonitoredVehicleJourney":
+                    buses.append(subChild)
+
+    # Filter buses
+    myBuses = []
+    for bus in buses:
+        for node in bus:
+            if node.tag == "LineRef":
+                # .text returns "polluted" text :-(
+                if(node.text.find("5") != -1):
+                   myBuses.append(bus)
+                break
+
+    # Extract orginally departure time (nice to have)
+    aimedTimes = []
+    expectedTimes = []
+
+    i = 0
+    for bus in myBuses:
+        for node in bus:
+            if node.tag == "MonitoredCall":
+                for subnode in node:
+                    if subnode.tag == "AimedDepartureTime":
+                        aimedTime = subnode.text
+                        aimedTime = aimedTime.replace("\n", "")
+                        aimedTime = aimedTime.replace(" ", "")
+                        aimedTimes.append((i, aimedTime))
+                        i = i + 1
+                    if subnode.tag == "ExpectedDepartureTime":
+                        expectedTime = subnode.text
+                        expectedTime = expectedTime.replace("\n", "")
+                        expectedTime = expectedTime.replace(" ", "")
+                        expectedTimes.append((i,expectedTime))
+
+    for item in aimedTimes:
+        print(item)
+
+    for item in expectedTimes:
+        print(item)
+
+
+
+
 
 def getArrivalTimes(tree, location):
     """Input: XML-tree and name of bus stop. Has to be the name used in AtB's XML.
@@ -77,8 +151,9 @@ def busStopForecast(stopId,namespace,url):
     f.close()
 
 def main():
+    #busStopForecast(STOP_ID,SIRI_NAMESPACE,SIRI_SM_SERVICE)
     tree = ET.parse('AtB.xml')
-    print(getArrivalTimes(tree, 'Pirbadet'))
+    print(getDepartureTimesAsveien(tree))
 
 if __name__ == '__main__':
     main()
