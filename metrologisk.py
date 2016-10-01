4a8a66d33590e46cda0206b69646441b5a5b9031
VLineLine <vegard.line@gmail.com>
2016-10-02 00:30:19 +0200

    Load img from a URL and display
    Stil not workind :/

---
+++ metrologisk.py
@@ -0,0 +1,37 @@
+﻿from PIL import Image
+from io import StringIO, BytesIO
+import urllib.request
+
+
+
+class getDataFormMet(object):
+
+    
+
+    def getRadarImage:
+        
+        #imgUrl="http://api.met.no/weatherapi/radar/1.5/?radarsite=central_norway;type=reflectivity;content=animation;size=normal"
+        imgUrl2='http://weknowyourdreams.com/images/egg/egg-01.jpg'
+        imgUrl3='http://python.org/'
+        
+        
+        radarResponse=urllib.request.urlopen(imgUrl2)
+        
+        #radarImg = Image.open(BytesIO(radarResponse.content))
+        
+        #radarImg = Image.open(StringIO(radarResponse.read()))
+
+        radarImg = Image.open(BytesIO(radarResponse.read())
+
+
+
+
+
+    #http://api.met.no/weatherapi/radar/1.5/documentation
+
+    
+    pass
+
+
+
+
