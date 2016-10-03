68443f5aa10141b6ee37cddb612cd987c4d6800e
VLineLine <vegard.line@gmail.com>
2016-10-03 23:26:04 +0200

    Try to play gif on pyqt or simular

--- metrologisk.py
+++ metrologisk.py
@@ -1,6 +1,6 @@
 ﻿from PIL import Image
 from io import StringIO, BytesIO
-import urllib.request
+from urllib.request import urlretrieve
 
 
 
@@ -10,18 +10,14 @@ class getDataFormMet(object):
 
     def getRadarImage:
         
-        #imgUrl="http://api.met.no/weatherapi/radar/1.5/?radarsite=central_norway;type=reflectivity;content=animation;size=normal"
+        imgUrl="http://api.met.no/weatherapi/radar/1.5/?radarsite=central_norway;type=reflectivity;content=animation;size=normal"
         imgUrl2='http://weknowyourdreams.com/images/egg/egg-01.jpg'
-        imgUrl3='http://python.org/'
         
-        
-        radarResponse=urllib.request.urlopen(imgUrl2)
-        
-        #radarImg = Image.open(BytesIO(radarResponse.content))
-        
-        #radarImg = Image.open(StringIO(radarResponse.read()))
 
-        radarImg = Image.open(BytesIO(radarResponse.read())
+        imgStr='radarimg' + '.gif'
+        urlretrieve(imgUrl, imgStr)
+        radarImg = Image.open(imgStr)
+        radarImg.show()
 
 
 
@@ -31,6 +27,8 @@ class getDataFormMet(object):
 
     
     pass
+
+
 
 
 
