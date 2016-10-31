28b8b352c7bf26db75b79ca5ebaf3ed7bb55a816
VLineLine <vegard.line@gmail.com>
2016-10-31 21:29:50 +0100

    Added activity class

--- strava_data.py
+++ strava_data.py
@@ -1,16 +1,28 @@
 ﻿from stravalib import Client
 
 import requests as r
+from datetime import datetime, timedelta
+import re
 #import httplib
 
 #from flask import Flask
 #from flask import request
 
+d=datetime.today()
 
+
+class strava_activity(object):
+    def __init__(self,name):
+        self.name=name                  #str of name
+        self.distance=0                 #m
+        self.time=timedelta(seconds=0)  #HH:MM:SS
+        self.kudos=0                    #nr
 
 class strava_class(object):
     def __init__(self):
         self.client = Client()
+
+
 
     def login_vegard(self):
         MY_STRAVA_CLIENT_ID=14139
@@ -28,22 +40,60 @@ class strava_class(object):
         #stop web server
         return 0
     
-    def get_last_week(self,lim):
-        for activity in self.client.get_activities(after="2016-08-01T00:00:00Z", limit=lim):
-            print("{0.name} {0.moving_time}".format(activity))
+    def get_last_days(self,days,lim):
+        week_ago=datetime.today()-timedelta(days=days)
+        week_ago_s=week_ago.strftime("%Y-%m-%dT%H:%M:%S:00Z")
+
+        act=[]              #Class to store the activitys
+        
+        types=['name','distance','moving_time','kudos_count']
+        #streams=self.client.get_activity_streams()
+        len=0
+        for ac in self.client.get_activities(after=week_ago_s, limit=lim):
+            #print("{0.name} {0.distance} {0.moving_time} {0.kudos_count}".format(ac))
+            act.append(strava_activity(name="{0.name}".format(ac)))
+            
+            act[-1].distance=float((re.findall("\d+\.\d+","{0.distance}".format(ac)))[-1])
+
+            t = datetime.strptime("{0.moving_time}".format(ac),"%H:%M:%S")
+            act[-1].time=timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+            
+            act[-1].kudos=int("{0.kudos_count}".format(ac))       
+
+        return act
+    
+#datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+#datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S:00Z")
 
 
 sc=strava_class()
 sc.login_vegard()
-sc.get_last_week(10)
+aa=sc.get_last_days(30,10)
+
+#Count kudos last mnth:
+tkudos=0
+tdistance=0
+ttime=timedelta(seconds=0)
+
+for ac in aa:
+    tkudos=tkudos+ac.kudos
+    tdistance=tdistance+ac.distance
+    ttime=ttime+ac.time
+
+print("Antal treningsøkter de siste 30 dagene:" + repr(len(aa)))
+print("Antall km de siste 30 dagene:" + repr(tdistance/1000))
+print("Antall kudos siste 30 dagene:" + repr(tkudos))
+print("Total treningstid de siste 30 dagene:" + str(ttime))
+
+
+#for acc in aa:
+#    print(acc('name'))
+
 #sc.login_strava()
 #sc.client
 
 
 #athlete=client.get_athlete()
-
-
-
 
 #url = client.authorization_url(client_id=MY_STRAVA_CLIENT_ID, redirect_uri=MY_Url)
 
@@ -57,7 +107,6 @@ sc.get_last_week(10)
 
 #code = request.args.get(url)
 #a=r.get(MY_Url)
-
 
 
 
