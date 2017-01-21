c7d9cc711db22deb34b8600e172227c1a8bf3f63
Johan Korsnes <johan.korsnes@gmail.com>
2017-01-22 00:43:44 +0100

    Pseudoserver WIP.

--- nextBus.py
+++ nextBus.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
 
 import datetime
+import time
 import xmlGet as xmlg
 import xmlParse as xmlp
 from bus import Buss
 
+# helper
 def timeDelta(t1, t2):
     s = t2.second - t1.second
     s += 60*(t2.minute - t1.minute)
     s += 3600*(t2.hour - t1.hour)
     return abs(s)
 
-if __name__ == '__main__':
-    xmlg.getXML();
+def nextBus():
+    xmlg.getXML()
     buses = xmlp.getBusObj()
 
-    times = []
     t1 = datetime.datetime.now().time()
+    lt = []
     for bus in buses:
         t2 = bus.getDepartureTimeSeconds()
-        times.append(timeDelta(t1, t2))
+        lt.append(timeDelta(t1, t2))
 
-    print(min(times))
+    # no departures?
+    if not lt:
+        return None
+
+    return min(lt)
+
+# set up loop in this block
+if __name__ == '__main__':
+    print(nextBus())
+    time.sleep(10)
+    print(nextBus())
